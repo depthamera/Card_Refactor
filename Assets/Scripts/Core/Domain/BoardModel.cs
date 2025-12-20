18116c084abe6961c2b0fc64ae4f99692127f12c
shet14886@gmail.com <shet14886@gmail.com>
2025-12-20 23:40:34 +0900

    refactor: add int2 overloads to BoardModel

--- Assets/Scripts/Core/Domain/BoardModel.cs
+++ Assets/Scripts/Core/Domain/BoardModel.cs
@@ -1,3 +1,5 @@
+using Unity.Mathematics;
+
 namespace CardRefactor.Core.Domain
 {
 	public class BoardModel
@@ -30,6 +32,8 @@ namespace CardRefactor.Core.Domain
             }
         }
 
+        public BoardModel(int2 size) : this(size.x, size.y) { }
+
         public BlockData GetBlock(int x, int y)
         {
             if(!IsValid(x, y))
@@ -40,9 +44,19 @@ namespace CardRefactor.Core.Domain
             return _grid[x, y];
         }
 
+        public BlockData GetBlock(int2 position)
+        {
+            return GetBlock(position.x, position.y);
+        }
+
         public bool IsValid(int x, int y)
         {
             return x >= 0 && x < Width && y >= 0 && y < Height;
+        }
+
+        public bool IsValid(int2 position)
+        {
+            return IsValid(position.x, position.y);
         }
 
         public void SetBlock(int x, int y, BlockData newBlockData)
@@ -58,6 +72,11 @@ namespace CardRefactor.Core.Domain
             }
 
             _grid[x, y] = newBlockData;
+        }
+
+        public void SetBlock(int2 position, BlockData newBlockData)
+        {
+            SetBlock(position.x, position.y, newBlockData);
         }
     }
 }
